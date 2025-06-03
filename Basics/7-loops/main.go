b0d9b8c43339249b46d3fa500fd573c42a89dca3
Fortune-Ndlovu <fndlovu@redhat.com>
2025-06-03 23:08:18 +0100

    loops
    Signed-off-by: Fortune-Ndlovu <fndlovu@redhat.com>

---
+++ Basics/7-loops/main.go
@@ -0,0 +1,24 @@
+package main
+import "fmt"
+
+func main () {
+	// While loop
+	// i := 1
+	// for i <= 10 {
+	// 	fmt.Println(i)
+	// 	i++
+	// }
+
+	//for loop
+	// for i := 1; i < 10; i++{
+	// 	fmt.Println(i)
+	// }
+
+	// inner loop
+	for i:=1; i<10; i++{
+		for j:=1; j<i; j++ {
+			fmt.Printf("*")
+		}
+		fmt.Println(i)
+	}
+}
