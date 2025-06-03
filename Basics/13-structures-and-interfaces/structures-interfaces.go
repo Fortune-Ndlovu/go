8244d3968a1c9fa957825eb2137adee1c4116299
Fortune-Ndlovu <fndlovu@redhat.com>
2025-06-04 00:24:06 +0100

    update
    Signed-off-by: Fortune-Ndlovu <fndlovu@redhat.com>

---
+++ Basics/13-structures-and-interfaces/structures-interfaces.go
@@ -0,0 +1,19 @@
+package main
+import "fmt"
+
+func main () {
+	rect1 := Rectangle{10, 5}
+	fmt.Println(rect1.height)
+	fmt.Println(rect1.width)
+
+	fmt.Println(rect1.area())
+}
+
+type Rectangle struct {
+	height float64
+	width float64
+}
+
+func (rec *Rectangle) area() float64 {
+	return rec.height * rec.width
+}
