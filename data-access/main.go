d03379d1d8dbe3bfaf1fdc5f80850280676349e0
Fortune-Ndlovu <fndlovu@redhat.com>
2025-07-13 16:25:06 +0100

    LastInsertedId
    Signed-off-by: Fortune-Ndlovu <fndlovu@redhat.com>

--- data-access/main.go
+++ data-access/main.go
@@ -54,6 +54,17 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Printf("Album found: %v\n", alb)
+
+	// call the AddAlbum function with a new album, assigning the ID of the album your adding to an albID variable
+	albID, err := addAlbum(Album{
+		Title: "The Modern Sound of Betty Carter",
+		Artist: "Betty Carter",
+		Price: 49.99,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("ID of added album: %v\n", albID)
 }
 
 // albumsByArtist queries for albums that have the specified artist name.
@@ -93,4 +104,18 @@ func albumByID(id int64) (Album, error) {
 		return alb, fmt.Errorf("albumsById %d: %v", id, err)
 	}
 	return alb, nil
+}
+
+// addAlbum add the specified album to the database,
+// returning the album ID of the new entry
+func addAlbum(alb Album) (int64, error) {
+	result, err := db.Exec("INSERT INTO album (title, artist, price) VALUES (?, ?, ?)", alb.Title, alb.Artist, alb.Price)
+	if err != nil {
+		return 0, fmt.Errorf("addAlbum: %v", err)
+	}
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, fmt.Errorf("addAlbum: %v", err)
+	}
+	return id, nil
 }
