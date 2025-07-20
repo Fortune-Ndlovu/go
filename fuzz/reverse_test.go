1a6424caec1b0d1540816e49d93d93eff0859808
Fortune-Ndlovu <fndlovu@redhat.com>
2025-07-20 16:07:50 +0100

    iterate over each rune
    Signed-off-by: Fortune-Ndlovu <fndlovu@redhat.com>

--- fuzz/reverse_test.go
+++ fuzz/reverse_test.go
@@ -13,8 +13,9 @@ func FuzzReverse(f *testing.F) {
 	f.Fuzz(func(t *testing.T, orig string) {
 		rev := Reverse(orig)
 		doubleRev := Reverse(rev)
+		t.Logf("Number of runes: orig=%d, rev=%d, doubleRev=%d", utf8.RuneCountInString(orig), utf8.RuneCountInString(rev), utf8.RuneCountInString(doubleRev))
 		if orig != doubleRev {
-			t.Errorf("Before: %q, after; %q", orig, doubleRev)
+			t.Errorf("Before: %q, after: %q", orig, doubleRev)
 		}
 		if utf8.ValidString(orig) && !utf8.ValidString(rev) {
 			t.Errorf("Reverse produced invalid UTF-8 string %q", rev)
