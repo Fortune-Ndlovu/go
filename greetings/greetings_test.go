3bbbfe260131581761cf821266a308a5e5b5d2b2
Fortune Ndlovu <ndlovufortune97@gmail.com>
2025-07-26 23:46:18 +0100

    Update greetings_test.go

--- greetings/greetings_test.go
+++ greetings/greetings_test.go
@@ -5,8 +5,7 @@ import (
     "regexp"
 )
 
-// TestHelloName calls greetings.Hello with a name, checking
-// for a valid return value.
+// TestHelloName calls greetings.Hello with a name, checking for a valid return value.
 func TestHelloName(t *testing.T) {
     name := "Gladys"
     want := regexp.MustCompile(`\b`+name+`\b`)
