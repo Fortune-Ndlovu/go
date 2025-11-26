7eeada337290f21b709c7a71d5703e957bed570e
Fortune Ndlovu <ndlovufortune97@gmail.com>
2025-11-26 23:33:08 +0000

    Update ugani.go

--- ugani/ugani.go
+++ ugani/ugani.go
@@ -6,6 +6,6 @@ import (
 )
 
 func main() {
- // reversing both a string type and an Int type
+  // reversing both a string type and an Int type
 	fmt.Println(reverse.String("Ugani Friend"), reverse.Int(24601))
 }
