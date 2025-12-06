998536ef570f8fd5ed6ef21f2afd7a46aba60a02
Fortune Ndlovu <fndlovu@redhat.com>
2025-12-06 21:22:03 +0000

    clean up everything
    Signed-off-by: Fortune Ndlovu <fndlovu@redhat.com>

--- Basics/1-Hello-World/first.go
+++
@@ -1,7 +0,0 @@
-package main
-import "fmt"
-
-func main () {
-	myName := "Fortune"
-	fmt.Println("Hello World! ", myName)
-}
--- Basics/2-variableAndConstants/error_change_constant.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-func errorChangeConstant () {
-	const name = "Fortune"
-	// name = "lion"
-	fmt.Println("My name: ", name)
-}
